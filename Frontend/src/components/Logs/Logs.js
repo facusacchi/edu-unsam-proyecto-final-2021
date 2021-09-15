6cf17ed54613ea4f46a8abafc9b5ce5433b010b3
facu sacchi <faq.ndo.n.s@gmail.com>
2021-09-14 23:55:05 -0300

    agregamos algunos icons

--- Frontend/src/components/Logs/Logs.js
+++ Frontend/src/components/Logs/Logs.js
@@ -93,7 +93,18 @@ const Logs = () => {
             </Form.Group>
           </Col>
           <Col className="button-col">
-            <Button variant={"primary"}>Buscar</Button>
+            <Button className='search-button' variant={"primary"}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </Button>
           </Col>
         </Row>
       </Form>
@@ -107,11 +118,22 @@ const Logs = () => {
             <TableBody />
           </tbody>
         </Table>
-      <div className='export-button' className="d-grid gap-2">
-        <Button variant="success" size="lg">
-          Exportar
-        </Button>
-      </div>
+        <div className="export-button" className="d-grid gap-2">
+          <Button variant="success" size="lg">
+            Exportar{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-download"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+              <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+            </svg>
+          </Button>
+        </div>
       </div>
     </>
   );
@@ -146,7 +168,21 @@ const TableBody = () => {
       <td>{log.evento}</td>
       <td>
         <Button onClick={() => deleteHandler(log.id)} variant={"danger"}>
-          Eliminar
+          Eliminar{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-trash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+            <path
+              fill-rule="evenodd"
+              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+            />
+          </svg>
         </Button>
       </td>
     </tr>
