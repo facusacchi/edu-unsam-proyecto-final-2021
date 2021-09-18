2ea1edc6f8c2958cc7e7686afd098fcbb00db977
facu sacchi <faq.ndo.n.s@gmail.com>
2021-09-18 01:20:43 -0300

    agrego pantalla de contacto

--- Frontend/src/components/Contacto/Contacto.js
+++ Frontend/src/components/Contacto/Contacto.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { Card, Form, Button } from "react-bootstrap";
+import './contacto.css';
 
 const Contacto = () => {
-    return(
-        <div>
-          <img src="" alt="" sizes="(max-width: 120px) 100vw, 120px"/>
-          <h1>CONTACTO</h1>
-          <div>Para cualquier consulta podes comunicarte con la casilla de correo indicada:</div>
-          <a>email@mail.com</a>
-        </div>
-    );
-}
+  return (
+    <Card className="card-contacto">
+      <Card.Body>
+        <Card.Title className="title-form frm-grp title-form">
+          Contacto
+        </Card.Title>
+        <Form>
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Nombre y apellido</Form.Label>
+            <Form.Control type="text" placeholder="Ingresar nombre completo" />
+          </Form.Group>
+
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="text" placeholder="Ingresar email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Repetir email</Form.Label>
+            <Form.Control type="text" placeholder="Ingresar email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Consulta</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+
+          <div className="button-form frm-grp">
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </div>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Contacto;
--- Frontend/src/components/Retrieve/Retrieve.js
+++ Frontend/src/components/Retrieve/Retrieve.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import {Form, Button, Row, Col, Card} from 'react-bootstrap';
-import './retrieve.css'
+import React from "react";
+import { Form, Button, Row, Col, Card } from "react-bootstrap";
+import "./retrieve.css";
 
 const Retrieve = () => {
-    return(
-        <Card className="card-reg card-rtv">
+  return (
+    <Card className="card-reg card-rtv">
       <Card.Body>
-        <Card.Title className='title-form frm-grp title-form'>Recuperar usuario/contraseña</Card.Title>
+        <Card.Title className="title-form frm-grp title-form">
+          Recuperar usuario/contraseña
+        </Card.Title>
         <Form>
-              <Form.Group className="mb-3 frm-grp" controlId="form-control">
-                <Form.Label>Email de registro</Form.Label>
-                <Form.Control type="text" placeholder="Ingresar email" />
-              </Form.Group>
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Email de registro</Form.Label>
+            <Form.Control type="text" placeholder="Ingresar email" />
+          </Form.Group>
 
-              <Form.Group className="mb-3 frm-grp" controlId="form-control">
-                <Form.Label>Repetir email</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingresar email"
-                />
-              </Form.Group>
+          <Form.Group className="mb-3 frm-grp" controlId="form-control">
+            <Form.Label>Repetir email</Form.Label>
+            <Form.Control type="text" placeholder="Ingresar email" />
+          </Form.Group>
 
           <div className="button-form frm-grp">
             <Button variant="primary" type="submit">
               Recuperar
             </Button>
           </div>
-          <div className='frm-grp'>
-              <a href='/login'>Volver al login</a>
-            </div>
+          <div className="frm-grp">
+            <a href="/login">Volver al login</a>
+          </div>
         </Form>
       </Card.Body>
     </Card>
-    );
-}
+  );
+};
 
 export default Retrieve;
